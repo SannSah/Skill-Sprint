647f78dd83a283997e80719117f84aaffdb5f9e3
shivam tiwari <shivamkrish149@gmail.com>
2024-03-30 12:12:19 +0530

    Checking Only

---
+++ BackEnd/Controllers/Admin/AdminSignin.js
@@ -0,0 +1,24 @@
+import Admin from "../../Models/AdminModel.js";
+import jwt from "jsonwebtoken";
+import secretKey from "../../SecretKey.js";
+
+async function adminSignin(req, res) {
+  const { username, password } = req.body;
+  
+  try {
+    const admin = await Admin.findOne({ name: username, password: password });
+
+    if (admin) {
+      const token = jwt.sign({ adminId: admin._id,D_id:admin.Department_id}, secretKey);
+      res.json({ isValid: true, token: token });
+      
+    } else {
+      res.json({ isValid: false });
+    }
+  } catch (error) {
+    console.error("Error during admin sign-in:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
+
+export default adminSignin;
---
+++ BackEnd/Middlewares/AuthenticationMiddleware.js
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken';
+import secretKey from '../SecretKey.js';
+
+function Authentication(req, res, next) {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.sendStatus(401);
+  }
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      console.error('JWT verification failed:', err);
+      return res.status(403).json({inValidToken:true});
+    } else {
+      
+      req.user = decoded;
+      next();
+    }
+  });
+}
+
+export default Authentication;
